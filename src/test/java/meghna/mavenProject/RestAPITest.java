ca42abafb696d0a4db1d69ede30666e67fa93b97
Meghna1391 <meghnapatel1391@yahoo.com>
2021-11-11 17:38:52 -0500

    second commit

--- src/test/java/meghna/mavenProject/RestAPITest.java
+++ src/test/java/meghna/mavenProject/RestAPITest.java
@@ -7,11 +7,17 @@ public class RestAPITest {
 	@Test
 	public void postJira() {
 		System.out.println("postJira");
+		System.out.println("postJira1");
+		System.out.println("postJira2");
 	}
 	
 	@Test
 	public void deleteTwitter() {
 		System.out.println("deleteTwitter");
+		System.out.println("deleteTwitter1");
+
+		System.out.println("deleteTwitter2");
+
 	}
 
 }
