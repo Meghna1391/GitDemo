86b68cee614abc2e75a2b0dbdb67d1f44a7b3521
Meghna1391 <meghnapatel1391@yahoo.com>
2021-11-11 14:52:50 -0500

    first commit

---
+++ src/test/java/meghna/mavenProject/SeleniumTest.java
@@ -0,0 +1,17 @@
+package meghna.mavenProject;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserTesting() {
+		System.out.println("browserTesting");
+	}
+	
+	@Test
+	public void uiElements() {
+		System.out.println("uiElements");
+	}
+
+}
